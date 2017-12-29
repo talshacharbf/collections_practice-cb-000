8e06263dd98353c1ba4a7e4385cc91ae7a526cf8
tal shachar <tal.shachar@buzzfeed.com>
2017-12-29 07:59:31 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,67 @@
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    if a == array[0]
+    0
+    elsif a == array[1]
+      if  a == b
+        1
+      elsif a < b
+        1
+      elsif a > b
+        1
+      end
+    else
+    0
+    end
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+  string[2]= "$"
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+  string[0]== "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+  sum + n
+  end
+end
+
+def add_s(words)
+  words.each do |word|
+    if word != words[1]
+      word << "s"
+    else
+    end
+  end
+end
